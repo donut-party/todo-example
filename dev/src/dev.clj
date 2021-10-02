60d7bfc1fbe1df4c5f9429003cad306b63ab709f
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2021-10-02 13:04:20 -0400

    even better auto reload

--- dev/src/dev.clj
+++ dev/src/dev.clj
@@ -2,23 +2,14 @@
   {:clj-kondo/config {:linters {:unused-namespace {:level :off}}}}
   (:require
    [clojure.tools.namespace.repl :as nsrepl]
+   [dev.hawk]
    [donut.system :as ds]
    [donut.system.repl :as dsr]
-   [donut.todo-example.backend.system]
-   [hawk.core :as hawk])
+   [donut.system.repl.state :as dsrs]
+   [donut.todo-example.backend.system])
   (:refer-clojure :exclude [test]))
 
 (nsrepl/set-refresh-dirs "dev/src" "src" "test")
 
-(defn auto-reset-handler [ctx _event]
-  (binding [*ns* *ns*]
-    (dsr/restart)
-    ctx))
-
-(defn- source-file? [_ {:keys [file]}]
-  (re-find #"(\.cljc?|\.edn)$" (.getName file)))
-
-(def hawk-watch!
-  (delay (hawk/watch! [{:paths   ["src/" "resourcs/" "dev/src/" "dev/resoruces/"]
-                        :filter  source-file?
-                        :handler auto-reset-handler}])))
+(when-not dsrs/system
+  (dsr/start))
