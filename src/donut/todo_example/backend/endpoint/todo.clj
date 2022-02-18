5c32163e5ce158479d0e0d29ac3131c58dd8ca40
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2022-02-18 10:48:24 -0500

    whitespace

--- src/donut/todo_example/backend/endpoint/todo.clj
+++ src/donut/todo_example/backend/endpoint/todo.clj
@@ -5,40 +5,44 @@
 (def handlers
   {;; "/todo"
    :collection
-   {:get (fn [{:keys [db]}]
-           {:status 200
-            :body   (->> {:select [:*]
-                          :from   [:todo]}
-                         sql/format
-                         (jsql/query db)
-                         (into []))})}
+   {:get
+    (fn [{:keys [db]}]
+      {:status 200
+       :body   (->> {:select [:*]
+                     :from   [:todo]}
+                    sql/format
+                    (jsql/query db)
+                    (into []))})}
 
    ;; "/todo/:id"
    :member
-   {:get {:parameters {:path [:map [:id int?]]}
-          :handler    (fn [{:keys [all-params db]}]
-                        {:status 200
-                         :body   (->> {:select [:*]
-                                       :from   [:todo]
-                                       :where  [:= :id (:id all-params)]}
-                                      sql/format
-                                      (jsql/query db)
-                                      (into []))})}
+   {:get
+    {:parameters {:path [:map [:id int?]]}
+     :handler    (fn [{:keys [all-params db]}]
+                   {:status 200
+                    :body   (->> {:select [:*]
+                                  :from   [:todo]
+                                  :where  [:= :id (:id all-params)]}
+                                 sql/format
+                                 (jsql/query db)
+                                 (into []))})}
 
-    :put {:parameters {:path [:map [:id int?]]}
-          :handler    (fn [{:keys [all-params db]}]
-                        (jsql/update! db
-                                      :todo
-                                      (dissoc all-params :id)
-                                      (select-keys all-params [:id]))
-                        {:status 200
-                         :body   (->> {:select [:*]
-                                       :from   [:todo]
-                                       :where  [:= :id (:id all-params)]}
-                                      sql/format
-                                      (jsql/query db)
-                                      (into []))})}
+    :put
+    {:parameters {:path [:map [:id int?]]}
+     :handler    (fn [{:keys [all-params db]}]
+                   (jsql/update! db
+                                 :todo
+                                 (dissoc all-params :id)
+                                 (select-keys all-params [:id]))
+                   {:status 200
+                    :body   (->> {:select [:*]
+                                  :from   [:todo]
+                                  :where  [:= :id (:id all-params)]}
+                                 sql/format
+                                 (jsql/query db)
+                                 (into []))})}
 
-    :delete {:parameters {:path [:map [:id int?]]}
-             :handler    (fn [{:keys [all-params db]}]
-                           (jsql/delete! db :todo (select-keys all-params [:id])))}}})
+    :delete
+    {:parameters {:path [:map [:id int?]]}
+     :handler    (fn [{:keys [all-params db]}]
+                   (jsql/delete! db :todo (select-keys all-params [:id])))}}})
